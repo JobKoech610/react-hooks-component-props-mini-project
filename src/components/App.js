9ab5f48d4a5b84023b5132a657912a56dd9ca3bb
Job koech <jobkoech610@gmail.com>
2023-04-21 15:57:10 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../components/Header"
+import About from "./About";
+import ArticleList from "./ArticleList";
+
 
 console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
+    
+<div>
+<Header blogName = {blogData.name}/>;
+<About image={blogData.image} about= {blogData.about}/>
+<ArticleList posts={blogData.posts}/>
+</div>
+    
+  )
+  
+  // return (
+  //   // <div className="App">
+  //   //   You're on your own from here! Follow the deliverables; test things out in
+  //   //   the browser as you write your code; and good luck!
+  //   // </div>
+  //   <Header />
+  // );
 }
 
+
+
 export default App;
+ 
