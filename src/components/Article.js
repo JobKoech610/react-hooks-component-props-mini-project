3b05c2d23571a7a542945d8d8f2f584b3a673ddf
Job koech <jobkoech610@gmail.com>
2023-04-28 11:53:59 +0300

    made changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,6 @@
 
-function Article(props){
-    const {title,date="January 1, 1970",preview} = props.post
+function Article({title,date="January 1, 1970",preview}){
+    // const {title,date="January 1, 1970",preview} = props.post
 return(
 <article>
 <h3>{title}</h3>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,7 +6,7 @@ const {posts}=props
     return(
         <main>
         {posts.map(post =>(
-            <Article key={post.id} post={post} />
+            <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
         ))}
     </main>
     )
